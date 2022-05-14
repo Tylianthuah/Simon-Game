4797bd28e2e82b6ce4c9ac6b07681b68cb6fca11
Tylianthuah <tylian15@gmail.com>
2022-05-14 19:45:26 +0530

    Second Commit

---
+++ game.js
@@ -0,0 +1,37 @@
+const buttonColors = ['red', 'blue', 'green', 'yellow'];
+var gamePattern = [];
+const userClickedPattern = [];
+
+$('.btn').click(function () {
+  var userChosenColor = $(this).attr('id');
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePressed(userChosenColor);
+});
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColor);
+
+  $('#' + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var audio = new Audio('sounds/' + name + '.mp3');
+  audio.play();
+}
+
+function animatePressed(currentColor) {
+  $('.' + currentColor).addClass('pressed');
+
+  setTimeout(function () {
+    $('.' + currentColor).removeClass('pressed');
+  }, 100);
+}
